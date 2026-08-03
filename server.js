f0f2a40d986d9b332ff9fc8bc15d4e50e1962ba0
mani1235-byte <cubegame2026@gmail.com>
2026-08-03 10:55:00 +0200

    Update server.js

--- server.js
+++ server.js
@@ -99,9 +99,9 @@ app.use((req, res, next) => {
   res.setHeader('Permissions-Policy',      'camera=(), microphone=(), geolocation=()');
   res.setHeader('Content-Security-Policy',
     "default-src 'self'; " +
-    "script-src 'self' 'unsafe-inline' https://www.gstatic.com https://cdn.jsdelivr.net https://cdnjs.cloudflare.com https://pagead2.googlesyndication.com https://adservice.google.com https://www.googletagservices.com https://www.googletagmanager.com https://scripts.simpleanalyticscdn.com; " +
-    "connect-src 'self' wss: ws: https://*.firebaseapp.com https://*.googleapis.com https://cubegame.club https://www.cubegame.club https://pagead2.googlesyndication.com https://adservice.google.com https://www.google-analytics.com https://www.googletagmanager.com https://scripts.simpleanalyticscdn.com https://queue.simpleanalyticscdn.com https://ep1.adtrafficquality.google https://www.gstatic.com; " +
-    "img-src 'self' data: https://*.googleusercontent.com https://pagead2.googlesyndication.com https://tpc.googlesyndication.com https://www.google-analytics.com; " +
+    "script-src 'self' 'unsafe-inline' https://www.gstatic.com https://cdn.jsdelivr.net https://cdnjs.cloudflare.com https://pagead2.googlesyndication.com https://adservice.google.com https://www.googletagservices.com https://www.googletagmanager.com https://scripts.simpleanalyticscdn.com https://ep1.adtrafficquality.google https://ep2.adtrafficquality.google; " +
+    "connect-src 'self' wss: ws: https://*.firebaseapp.com https://*.googleapis.com https://cubegame.club https://www.cubegame.club https://pagead2.googlesyndication.com https://adservice.google.com https://www.google-analytics.com https://*.analytics.google.com https://www.googletagmanager.com https://scripts.simpleanalyticscdn.com https://queue.simpleanalyticscdn.com https://ep1.adtrafficquality.google https://ep2.adtrafficquality.google https://www.gstatic.com; " +
+    "img-src 'self' data: https://*.googleusercontent.com https://pagead2.googlesyndication.com https://tpc.googlesyndication.com https://www.google-analytics.com https://queue.simpleanalyticscdn.com; " +
     "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; " +
     "font-src 'self' https://fonts.gstatic.com; " +
     "frame-src https://googleads.g.doubleclick.net https://tpc.googlesyndication.com https://pagead2.googlesyndication.com; " +
