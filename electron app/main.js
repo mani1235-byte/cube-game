1445becedad9260f213627a37d93cc7c8d728c57
cube game <cubegame2026@gmail.com>
2026-08-19 16:36:16 +0200

    initial commit

--- electron app/main.js
+++ electron app/main.js
@@ -1,9 +1,11 @@
 const { app, BrowserWindow } = require("electron");
+const path = require("path");
 
 function createWindow() {
     const win = new BrowserWindow({
         width: 1400,
-        height: 900
+        height: 900,
+        icon: path.join(__dirname, "cube_game_logo.ico")
     });
 
     win.loadURL("https://cubegame.club");
